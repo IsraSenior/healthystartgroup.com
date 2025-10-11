42623dab06e266ad0e89ae3d103541fd2e3353ef
Israel Senior <israsenior.dev@gmail.com>
2025-10-11 08:31:16 -0500

    @nuxthq/studio

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,10 +5,25 @@ export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
   css: ["~/assets/css/fonts.css", "~/assets/css/tailwind.css"],
-  modules: ["@nuxt/content"],
+  modules: ["@nuxthq/studio", "@nuxt/content"],
   vite: {
     plugins: [tailwindcss()],
   },
+  studio: {
+    enabled: true,
+    dev: true,
+  },
+  runtimeConfig: {
+    public: {
+      siteUrl: "https://healthystartgroup.nuxt.space",
+    },
+  },
+
+  content: {
+    preview: {
+      api: 'https://healthystartgroup.nuxt.space'
+    }
+  }
 
   // i18n: {
   //   locales: [
